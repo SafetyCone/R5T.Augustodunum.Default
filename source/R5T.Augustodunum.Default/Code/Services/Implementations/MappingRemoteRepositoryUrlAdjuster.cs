3cc873633105b0f36cc262f7906d829a6a4f7ced
MinexAutomation <minex.automation@gmail.com>
2019-12-22 23:38:13 -0800

    Implemented mapping remote repository URL adjuster.

---
+++ source/R5T.Augustodunum.Default/Code/Services/Implementations/MappingRemoteRepositoryUrlAdjuster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using R5T.Magyar.Extensions;
+
+
+namespace R5T.Augustodunum.Default
+{
+    public class MappingRemoteRepositoryUrlAdjuster : IMappingRemoteRepositoryUrlAdjuster
+    {
+        public string AdjustRemoteRepositoryUrl(string repositoryUrl)
+        {
+            // Adjust for possible SVN-style repository URLs.
+            var lastSix = repositoryUrl.Last(6);
+
+            var trunkTerminated = lastSix == "/trunk";
+
+            var trunkAdjustedUrl = trunkTerminated ? repositoryUrl.ExceptLast(6) : repositoryUrl;
+
+            // Ensure is git-style repository URL.
+            var lastFour = trunkAdjustedUrl.Last(4);
+
+            var gitTerminated = lastFour == ".git";
+
+            if (!gitTerminated)
+            {
+                throw new Exception($"Repository URL was NOT \".git\" terminated: {repositoryUrl}");
+            }
+
+            return trunkAdjustedUrl;
+        }
+    }
+}
