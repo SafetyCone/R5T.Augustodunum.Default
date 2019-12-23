eda6b73d0ae52e7c013e5aa251e19092b47d8ff9
MinexAutomation <minex.automation@gmail.com>
2019-12-22 23:39:42 -0800

    Added an SVN source control local directory path adjuster implementation.

---
+++ source/R5T.Augustodunum.Default/Code/Services/Implementations/SvnSourceControlProtocolRepositoryLocalDirectoryPathAdjuster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Augustodunum.Default
+{
+    public class SvnSourceControlProtocolRepositoryLocalDirectoryPathAdjuster : ISourceControlProtocolRepositoryLocalDirectoryPathAdjuster
+    {
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public SvnSourceControlProtocolRepositoryLocalDirectoryPathAdjuster(IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string AdjustLocalDirectoryPath(string localDirectoryPath)
+        {
+            var adjustedPath = this.StringlyTypedPathOperator.EnsureNotDirectoryIndicatedPath(localDirectoryPath);
+            return adjustedPath;
+        }
+    }
+}
