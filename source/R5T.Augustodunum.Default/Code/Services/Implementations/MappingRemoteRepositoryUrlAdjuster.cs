0e046d94d37bc37585e0275ded4e584368fbb1ab
davidcoats <david@getsafetycone.com>
2022-12-27 11:10:13 -0800

    Interim changes.

--- source/R5T.Augustodunum.Default/Code/Services/Implementations/MappingRemoteRepositoryUrlAdjuster.cs
+++ source/R5T.Augustodunum.Default/Code/Services/Implementations/MappingRemoteRepositoryUrlAdjuster.cs
@@ -4,8 +4,9 @@ using R5T.Magyar.Extensions;using R5T.T0064;
 
 
 namespace R5T.Augustodunum.Default
-{[ServiceImplementationMarker]
-    public class MappingRemoteRepositoryUrlAdjuster : IMappingRemoteRepositoryUrlAdjuster,IServiceImplementation
+{
+    [ServiceImplementationMarker]
+    public class MappingRemoteRepositoryUrlAdjuster : IMappingRemoteRepositoryUrlAdjuster, IServiceImplementation
     {
         public string AdjustRemoteRepositoryUrl(string repositoryUrl)
         {
@@ -16,15 +17,15 @@ namespace R5T.Augustodunum.Default
 
             var trunkAdjustedUrl = trunkTerminated ? repositoryUrl.ExceptLast(6) : repositoryUrl;
 
-            // Ensure is git-style repository URL.
-            var lastFour = trunkAdjustedUrl.Last(4);
+            //// Ensure is git-style repository URL.
+            //var lastFour = trunkAdjustedUrl.Last(4);
 
-            var gitTerminated = lastFour == ".git";
+            //var gitTerminated = lastFour == ".git";
 
-            if (!gitTerminated)
-            {
-                throw new Exception($"Repository URL was NOT \".git\" terminated: {repositoryUrl}");
-            }
+            //if (!gitTerminated)
+            //{
+            //    throw new Exception($"Repository URL was NOT \".git\" terminated: {repositoryUrl}");
+            //}
 
             return trunkAdjustedUrl;
         }
